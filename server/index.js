2df0f9c2f84ce959bb05be8e1d16d006dfc66767
Pawel L <virtuozoo@o2.pl>
2021-03-13 18:18:23 +0100

    add basic node server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const CONNECTION_URL = process.env.DB_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log('error.message !!!!'));
+
+mongoose.set('useFindAndModify', false);
