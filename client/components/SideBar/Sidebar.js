7d3baecf264adabe15d19e9434972d9c8dae23c0
Pawel L <virtuozoo@o2.pl>
2021-03-19 15:07:24 +0100

    add basic styles on sidebar

--- client/components/SideBar/Sidebar.js
+++ client/components/SideBar/Sidebar.js
@@ -1,5 +1,33 @@
+import Link from 'next/link';
 import styles from '../SideBar/sideBar.module.css';
 
 export default function SideBar() {
-  return <aside className={styles.sidebar}>ASIDE</aside>;
+  return (
+    <aside className={styles.sidebar}>
+      <nav className={styles.sidebar__nav}>
+        <ul className={styles.sidebar__list}>
+          <li className={styles.sidebar__item}>
+            <Link href="/product/name">
+              <a className={styles.sidebar__link}>T-shirt</a>
+            </Link>
+          </li>
+          <li className={styles.sidebar__item}>
+            <Link href="/product/name">
+              <a className={styles.sidebar__link}>Jeans</a>
+            </Link>
+          </li>
+          <li className={styles.sidebar__item}>
+            <Link href="/product/name">
+              <a className={styles.sidebar__link}>Polo</a>
+            </Link>
+          </li>
+          <li className={styles.sidebar__item}>
+            <Link href="/product/name">
+              <a className={styles.sidebar__link}>Shoes</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
